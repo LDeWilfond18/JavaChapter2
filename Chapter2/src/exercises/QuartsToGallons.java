24e39d16a7fb65710e3168464c17da35c233db48
LDeWilfond18 <42647328+LDeWilfond18@users.noreply.github.com>
2018-09-04 09:53:26 -0500

    work

---
+++ Chapter2/src/exercises/QuartsToGallons.java
@@ -0,0 +1,16 @@
+package exercises;
+
+public class QuartsToGallons {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String totalString;
+		
+		int gallons;
+		int quarts;
+		
+		
+	}
+
+}
